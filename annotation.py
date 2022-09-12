68b5c551c17231530da2434eabf86b9e671c0546
RealTourani <m.tooorani@gmail.com>
2022-09-12 00:43:58 -0700

    Append bounding box details to a txt file

--- annotation.py
+++ annotation.py
@@ -4,8 +4,9 @@ import pybboxes as pbx
 
 
 #image_path
+details = []
 img_path="2.jpg"
-
+name , ext = img_path.split('.')
 #read image
 img_raw = cv2.imread(img_path)
 
@@ -17,10 +18,21 @@ channels = img_raw.shape[2]
 roi = cv2.selectROI(img_raw)
 
 #print rectangle points of selected roi
-print(roi)
 
 a = pbx.convert_bbox(roi, from_type="coco", to_type="yolo", image_size=(w,h))
-print(a)
+
+for i in a:
+    details.append(i)
+
+upperx = details[0]
+uppery = details[1]
+bottomx = details[2]
+bottmy = details[3]
+
+f = open(name+".txt", "w")
+f.write(str(upperx) + " "+ str(uppery) + " "+ str(bottomx)+ " "+ str(bottmy))
+f.close
+
 #Crop selected roi from raw image
 roi_cropped = img_raw[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
 
