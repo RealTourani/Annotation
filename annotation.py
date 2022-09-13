9ec3618c3ebcbb9117dcb16e4f010335ca9082be
RealTourani <m.tooorani@gmail.com>
2022-09-12 21:15:41 -0700

    Label all images in folder and save txt file per image

--- annotation.py
+++ annotation.py
@@ -1,45 +1,58 @@
+import os
 import cv2
-import numpy as np
 import pybboxes as pbx
 
 
-#image_path
-details = []
-img_path="2.jpg"
-name , ext = img_path.split('.')
-#read image
-img_raw = cv2.imread(img_path)
 
-h = img_raw.shape[0]
-w = img_raw.shape[1]
-channels = img_raw.shape[2]
+def labeling(img_path):
+    details = []
+    img_raw = cv2.imread(img_path)  # Read image
+    name , ext = img_path.split('.')  # Split the image name and the extension
+    
+    # Getting width, height and channels of image
+    h = img_raw.shape[0]
+    w = img_raw.shape[1]
+    channels = img_raw.shape[2]
 
-#select ROI function
-roi = cv2.selectROI(img_raw)
+    #select ROI function
+    roi = cv2.selectROI(img_raw)
 
-#print rectangle points of selected roi
+    #print rectangle points of selected roi
 
-a = pbx.convert_bbox(roi, from_type="coco", to_type="yolo", image_size=(w,h))
+    # Convert ROI coordinates to YOLO v3 format
+    ROItoYolo = pbx.convert_bbox(roi, from_type="coco", to_type="yolo", image_size=(w,h))
 
-for i in a:
-    details.append(i)
+    #Crop selected roi from raw image
+    roi_cropped = img_raw[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
 
-upperx = details[0]
-uppery = details[1]
-bottomx = details[2]
-bottmy = details[3]
+    for i in ROItoYolo:
+        details.append(i) # Append Yolo format coordinates to a list
 
-f = open(name+".txt", "w")
-f.write(str(upperx) + " "+ str(uppery) + " "+ str(bottomx)+ " "+ str(bottmy))
-f.close
 
-#Crop selected roi from raw image
-roi_cropped = img_raw[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
+    upperx = details[0]
+    uppery = details[1]
+    bottomx = details[2]
+    bottmy = details[3]
 
-#show cropped image
-cv2.imshow("ROI", roi_cropped)
+    
+    # Append coordinates to a txt file
+    f = open(name+".txt", "w")
+    f.write(str(upperx) + " "+ str(uppery) + " "+ str(bottomx)+ " "+ str(bottmy))
+    f.close
 
-cv2.imwrite("crop.jpeg",roi_cropped)
+    #show cropped image
+    cv2.imshow("ROI", roi_cropped)
 
-#hold window
-cv2.waitKey(0)
+    cv2.imwrite("crop.jpeg",roi_cropped)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+directory = 'frames/'
+number_of_files = len([item for item in os.listdir(directory) if os.path.isfile(os.path.join(directory, item))])
+
+for i in range(number_of_files):
+
+    labeling('frames/frame{}.jpg'.format(i))
+        
