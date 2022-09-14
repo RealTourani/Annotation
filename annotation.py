cdd17037962aef2d5e6f88a1a229ebf265ea25eb
RealTourani <m.tooorani@gmail.com>
2022-09-14 12:39:33 +0430

    getting first image of dataset and crop the ROI via user

--- annotation.py
+++ annotation.py
@@ -1,10 +1,5 @@
 import os
 import cv2
-import pybboxes as pbx
-
-names = [x for x in input("Enter the names : ").split()]
-with open('frames/classes.txt', mode='w') as myfile:
-    myfile.write('\n'.join(names))
 
 def labeling(img_path):
     details = []
@@ -18,38 +13,12 @@ def labeling(img_path):
 
     #select ROI function
     roi = cv2.selectROI(img_raw)
-
-    #print rectangle points of selected roi
-
-    # Convert ROI coordinates to YOLO v3 format
-    ROItoYolo = pbx.convert_bbox(roi, from_type="coco", to_type="yolo", image_size=(w,h))
-
+    
     #Crop selected roi from raw image
     roi_cropped = img_raw[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
 
-    for i in ROItoYolo:
-        details.append(i) # Append Yolo format coordinates to a list
-
-
-    upperx = details[0]
-    uppery = details[1]
-    bottomx = details[2]
-    bottmy = details[3]
-
-    f = open('frames/classes.txt', 'r')
-    data = f.read()
-    lines = data.splitlines()
-    class_idx = None
-    for idx,line in enumerate(lines):
-        if current_class == line:
-            class_idx = idx
-    
-    # Append coordinates to a txt file
-    f = open(name+".txt", "w")
-    f.write(str(class_idx) + " " + str(upperx) + " "+ str(uppery) + " "+ str(bottomx)+ " "+ str(bottmy))
-    f.close
-
     #show cropped image
+    cv2.imwrite('cropped.jpg',roi_cropped)
     cv2.imshow(name, roi_cropped)
 
     cv2.waitKey(0)
@@ -59,8 +28,6 @@ def labeling(img_path):
 directory = 'frames/'
 number_of_files = len([item for item in os.listdir(directory) if os.path.isfile(os.path.join(directory, item))])
 
-for i in range(10):
-    current_class = input("Enter your current class name : ")
-    labeling('frames/frame{}.jpg'.format(i))
+for i in range(1):
     
-        
+    labeling('frames/frame{}.jpg'.format(i))
